d126ddc44ac41f1b7ad3fbca1939fc3f1d592be0
sobol <nppstcc1001050311@gmail.com>
2023-09-04 19:34:11 +0800

    補齊token過期與格式不正確相關Exception

--- src/role/role.guard.ts
+++ src/role/role.guard.ts
@@ -6,10 +6,11 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import * as jwt from 'jsonwebtoken';
 import { Observable } from 'rxjs';
 import { RoleService } from './role.service';
-import { JwtService } from '@nestjs/jwt';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class RoleGuard implements CanActivate {
@@ -23,36 +24,51 @@ export class RoleGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request: Request = context.switchToHttp().getRequest();
+
     if (
       request.headers['authorization'] === null ||
       request.headers['authorization'] === undefined
     ) {
-      throw new HttpException(
-        'token is not valid or empty',
-        HttpStatus.UNAUTHORIZED,
-      );
+      throw new HttpException('token empty', HttpStatus.UNAUTHORIZED);
     }
+
     const jwtToken = (request.headers['authorization'] as string).replace(
       'Bearer ',
       '',
     );
-    const user = this.jwtService.verify(jwtToken, {
-      secret: this.configService.get('jwt.secret'),
-    });
-    // (request as any).user = {
-    //   role: 'admin',
-    // }; // 塞假資料來實測驗證功能
-    const { path, method } = request as any;
-    const action = this.RoleService.mappingAction(method);
 
-    if (!user) {
-      throw new UnauthorizedException();
+    try {
+      // 塞假資料來實測驗證功能
+      // (request as any).user = {
+      //   role: 'admin',
+      // };
+
+      const user = this.jwtService.verify(jwtToken, {
+        secret: this.configService.get('jwt.secret'),
+      });
+      if (!user) {
+        throw new UnauthorizedException();
+      }
+
+      const { path, method } = request as any;
+      const action = this.RoleService.mappingAction(method);
+
+      return this.RoleService.checkPermission(
+        `role:${user['role']}`,
+        path,
+        action,
+      );
+    } catch (err) {
+      if (err instanceof jwt.TokenExpiredError) {
+        throw new HttpException(
+          'token已過期，請重新登入',
+          HttpStatus.UNAUTHORIZED,
+        );
+      }
+
+      if (err instanceof jwt.JsonWebTokenError) {
+        throw new HttpException('token格式不正確', HttpStatus.UNAUTHORIZED);
+      }
     }
-
-    return this.RoleService.checkPermission(
-      `role:${user['role']}`,
-      path,
-      action,
-    );
   }
 }
