ab1f44ea7ab64dca0110783e91eeb062ab837993
sobol <C109152304@nkust.edu.tw>
2023-09-05 20:49:03 +0800

    no message

--- src/config/jwtSecret.config.ts
+++ src/config/jwtSecret.config.ts
@@ -1,5 +1,4 @@
 import { ConfigService, registerAs } from '@nestjs/config';
-import yamlConfig from './app.yamlConfig';
 
 export default registerAs('secrets', () => {
   const configService = new ConfigService();
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MultiHttpExceptionFilter } from './common/MultiHttpExceptionFilter';
 
 async function bootstrap() {
-  // enable cors
-  const app = await NestFactory.create(AppModule, { cors: true });
-  // const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
+
+  const configService = app.get<ConfigService>(ConfigService);
+  const cors = configService.get('cors');
+  if (cors) {
+    app.enableCors();
+  }
 
   app.useGlobalPipes(new ValidationPipe());
-  // app.useGlobalFilters(new jwtExpiredFilter());
   app.useGlobalFilters(new MultiHttpExceptionFilter());
   await app.listen(5000);
 }
