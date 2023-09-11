9b74a2aba32daa4e17fb11d8ed029097ff5a922e
sobol <C109152304@nkust.edu.tw>
2023-09-11 21:02:22 +0800

    no message

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-empty-function': 'off',
-    'prefer-rest-params':'off'
+    'prefer-rest-params':'off',
+    'no-unused-vars':'off',
   },
 };
