87e0385c8c03a187af02f2de02c29b74ad3ef9c2
sobol <C109152304@nkust.edu.tw>
2023-10-18 20:34:13 +0800

    no message

--- src/meeting-member/meeting-member.module.ts
+++ src/meeting-member/meeting-member.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
-import { MeetingMemberService } from './meeting-member.service';
-import { MeetingMemberController } from './meeting-member.controller';
+import { JwtService } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { MeetingMember } from './entities/meeting-member.entity';
 import { Meeting } from 'src/meeting/entities/meeting.entity';
 import { User } from 'src/user/entities/user.entity';
-import { JwtService } from '@nestjs/jwt';
+import { MeetingMember } from './entities/meeting-member.entity';
+import { MeetingMemberController } from './meeting-member.controller';
+import { MeetingMemberService } from './meeting-member.service';
 
 @Module({
   controllers: [MeetingMemberController],
-  imports: [TypeOrmModule.forFeature([MeetingMember, Meeting, User])],
+  imports: [TypeOrmModule.forFeature([Meeting, MeetingMember, User])],
   providers: [MeetingMemberService, JwtService],
 })
 export class MeetingMemberModule {}
