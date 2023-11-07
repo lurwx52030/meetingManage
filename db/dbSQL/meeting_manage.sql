374c258a4a47b004d1d1ff2ef55814ccd228674c
sobol <nppstcc1001050311@gmail.com>
2023-11-07 14:59:31 +0800

    update db

--- db/dbSQL/meeting_manage.sql
+++ db/dbSQL/meeting_manage.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- 主機： db:3306
--- 產生時間： 2023 年 10 月 20 日 12:44
--- 伺服器版本： 5.7.43
--- PHP 版本： 8.2.8
+-- 主機： containers-us-west-137.railway.app:5510
+-- 產生時間： 2023 年 11 月 07 日 06:55
+-- 伺服器版本： 8.1.0
+-- PHP 版本： 8.2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,7 +20,7 @@ SET time_zone = "+00:00";
 --
 -- 資料庫： `meeting_manage`
 --
-CREATE DATABASE IF NOT EXISTS `meeting_manage` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `meeting_manage` DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci;
 USE `meeting_manage`;
 
 -- --------------------------------------------------------
@@ -37,23 +37,39 @@ CREATE TABLE `meeting` (
   `end` datetime NOT NULL,
   `meetingRoomId` varchar(255) DEFAULT NULL,
   `creatorId` varchar(255) DEFAULT NULL,
-  `isCheckin` tinyint(4) NOT NULL DEFAULT '0',
-  `checkLimit` int(11) NOT NULL DEFAULT '5',
-  `isCheckout` tinyint(4) NOT NULL DEFAULT '0',
-  `notificationTime` int(11) DEFAULT NULL,
+  `isCheckin` tinyint NOT NULL DEFAULT '0',
+  `checkLimit` int NOT NULL DEFAULT '5',
+  `isCheckout` tinyint NOT NULL DEFAULT '0',
+  `notificationTime` int DEFAULT NULL,
   `createTime` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- 傾印資料表的資料 `meeting`
 --
 
 INSERT INTO `meeting` (`id`, `name`, `start`, `end`, `meetingRoomId`, `creatorId`, `isCheckin`, `checkLimit`, `isCheckout`, `notificationTime`, `createTime`) VALUES
-('M056', 'rrr', '2023-10-18 21:07:00', '2023-10-18 22:07:00', 'A001', 'A124', 0, 5, 0, 15, '2023-10-18 20:08:11'),
+('M056', 'rrr', '2023-10-25 20:30:00', '2023-10-18 22:07:00', 'A001', 'A124', 0, 5, 0, 15, '2023-10-25 20:17:54'),
 ('M057', 'aaa', '2023-10-19 22:19:00', '2023-10-19 23:19:00', 'A001', 'A124', 0, 5, 0, 0, '2023-10-19 21:19:57'),
 ('M058', 'ddda', '2023-10-20 21:23:00', '2023-10-20 22:23:00', 'A001', 'A121', 0, 5, 0, 0, '2023-10-19 21:23:19'),
-('M059', '999', '2023-10-21 07:00:00', '2023-10-21 11:00:00', 'A001', 'A121', 0, 5, 0, 15, '2023-10-20 00:00:53'),
-('M060', 'ttt', '2023-10-20 03:53:00', '2023-10-20 10:55:00', 'A001', 'A121', 0, 5, 0, 5, '2023-10-20 00:45:47');
+('M059', '999', '2023-10-31 20:37:00', '2023-10-31 11:00:00', 'A001', 'A121', 0, 5, 0, 15, '2023-10-31 20:21:20'),
+('M060', 'ttt', '2023-10-20 03:53:00', '2023-10-20 10:55:00', 'A001', 'A121', 0, 5, 0, 5, '2023-10-20 00:45:47'),
+('M061', 'abc', '2023-10-26 00:45:00', '2023-10-25 20:00:00', 'A001', 'A121', 0, 5, 0, 10, '2023-10-26 00:06:00'),
+('M063', 'qwerw', '2023-10-26 20:55:00', '2023-10-26 22:00:00', 'A001', 'A124', 0, 5, 0, 15, '2023-10-26 20:38:00'),
+('M064', 'aaa', '2023-10-31 14:45:00', '2023-10-31 15:45:00', 'A002', 'A124', 0, 5, 0, 5, '2023-10-26 20:50:54'),
+('M065', 'ttt', '2023-10-30 19:58:00', '2023-10-30 22:15:00', 'A001', 'A124', 0, 5, 0, 5, '2023-10-26 21:09:36'),
+('M066', 'ppp', '2023-10-29 22:15:00', '2023-10-27 10:30:00', 'A001', 'A124', 0, 5, 0, 10, '2023-10-29 23:07:05'),
+('M072', 'wqe', '2023-10-30 23:35:00', '2023-10-31 08:30:00', 'A002', 'A124', 0, 5, 0, 10, '2023-10-31 15:31:24'),
+('M073', 'www', '2023-10-31 10:30:00', '2023-10-31 13:30:00', 'A002', 'A124', 0, 5, 0, 0, '2023-10-31 15:33:06'),
+('M074', 'qwer', '2023-10-31 07:50:00', '2023-10-31 08:40:00', 'A002', 'A124', 0, 5, 0, 0, '2023-10-31 15:33:34'),
+('M076', 'www', '2023-10-31 07:50:00', '2023-10-31 08:50:00', 'A001', 'A124', 0, 5, 0, 0, '2023-10-31 15:44:06'),
+('M080', 'ddddd', '2023-10-31 08:05:00', '2023-10-31 11:05:00', 'A001', 'A124', 0, 5, 0, 0, '2023-10-31 16:03:33'),
+('M086', 'rrr', '2023-10-31 08:40:00', '2023-10-31 14:40:00', 'A001', 'A124', 0, 5, 0, 0, '2023-10-31 16:36:24'),
+('M088', 'ttt', '2023-10-31 20:45:00', '2023-10-31 22:45:00', 'A002', 'A124', 0, 5, 0, 0, '2023-10-31 16:42:24'),
+('M089', 'd', '2023-10-31 16:45:00', '2023-10-31 17:45:00', 'A002', 'A124', 0, 5, 0, 0, '2023-10-31 16:44:40'),
+('M092', '123', '2023-10-31 21:34:00', '2023-10-31 22:28:00', 'A001', 'A121', 0, 5, 0, 5, '2023-10-31 21:28:43'),
+('M093', '456', '2023-11-01 21:13:00', '2023-11-01 22:12:00', 'A002', 'A123', 0, 5, 0, 0, '2023-11-01 21:12:44'),
+('M095', '789', '2023-11-02 14:22:00', '2023-11-02 15:15:00', 'A001', 'A121', 0, 5, 0, 5, '2023-11-02 14:15:59');
 
 --
 -- 觸發器 `meeting`
@@ -75,20 +91,43 @@ DELIMITER ;
 
 DROP TABLE IF EXISTS `meeting_member`;
 CREATE TABLE `meeting_member` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `participantId` varchar(255) DEFAULT NULL,
   `meetingId` varchar(255) DEFAULT NULL,
   `singin` datetime DEFAULT NULL,
-  `singout` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `singout` datetime DEFAULT NULL,
+  `remark` varchar(255) DEFAULT NULL COMMENT '備註'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- 傾印資料表的資料 `meeting_member`
 --
 
-INSERT INTO `meeting_member` (`id`, `participantId`, `meetingId`, `singin`, `singout`) VALUES
-(45, 'A121', 'M056', NULL, NULL),
-(46, 'P555', 'M056', NULL, NULL);
+INSERT INTO `meeting_member` (`id`, `participantId`, `meetingId`, `singin`, `singout`, `remark`) VALUES
+(45, 'A121', 'M056', NULL, NULL, NULL),
+(46, 'P555', 'M056', NULL, NULL, NULL),
+(47, 'A121', 'M061', NULL, NULL, NULL),
+(50, 'A121', 'M063', NULL, NULL, NULL),
+(51, 'P555', 'M063', NULL, NULL, NULL),
+(52, 'A121', 'M064', '2023-10-26 21:24:06', NULL, NULL),
+(53, 'P555', 'M064', NULL, NULL, NULL),
+(54, 'R024', 'M064', NULL, NULL, NULL),
+(55, 'A121', 'M065', NULL, NULL, NULL),
+(56, 'P555', 'M065', NULL, NULL, NULL),
+(57, 'R024', 'M065', NULL, NULL, NULL),
+(58, 'A121', 'M066', '2023-10-27 00:25:32', NULL, NULL),
+(59, 'P555', 'M066', NULL, NULL, 'test remark4-knn'),
+(60, 'R024', 'M066', '2023-10-27 00:25:35', '2023-10-29 21:51:10', NULL),
+(61, 'A124', 'M059', '2023-10-29 01:22:13', '2023-10-29 21:48:27', NULL),
+(62, 'A121', 'M089', NULL, NULL, 'test remark M089 1'),
+(63, 'P555', 'M089', '2023-10-31 16:45:42', '2023-10-31 16:46:00', 'test remark M089 2'),
+(64, 'R024', 'M089', '2023-10-31 16:45:45', '2023-10-31 16:46:03', 'test remark M089 3'),
+(68, 'A123', 'M092', '2023-10-31 21:34:24', NULL, NULL),
+(69, 'A124', 'M092', '2023-10-31 21:34:51', NULL, NULL),
+(71, 'A123', 'M089', NULL, NULL, NULL),
+(72, 'A124', 'M093', NULL, NULL, '生病'),
+(73, 'A121', 'M093', NULL, NULL, NULL),
+(75, 'A123', 'M095', '2023-11-02 14:22:50', NULL, '生病');
 
 -- --------------------------------------------------------
 
@@ -101,7 +140,7 @@ CREATE TABLE `meeting_room` (
   `id` varchar(255) NOT NULL,
   `name` varchar(15) NOT NULL COMMENT '會議室名稱',
   `location` varchar(15) NOT NULL COMMENT '會議室位置'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- 傾印資料表的資料 `meeting_room`
@@ -109,7 +148,10 @@ CREATE TABLE `meeting_room` (
 
 INSERT INTO `meeting_room` (`id`, `name`, `location`) VALUES
 ('A001', 'MA324', '管一三樓'),
-('A002', 'MA212', '管一二樓');
+('A002', 'MA212', '管一二樓'),
+('A003', 'MA322', '管一三樓'),
+('A004', 'MA320', '管一三樓'),
+('A005', 'MB333', '管二三樓');
 
 -- --------------------------------------------------------
 
@@ -119,8 +161,8 @@ INSERT INTO `meeting_room` (`id`, `name`, `location`) VALUES
 
 DROP TABLE IF EXISTS `meeting_sequence`;
 CREATE TABLE `meeting_sequence` (
-  `id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- 傾印資料表的資料 `meeting_sequence`
@@ -186,7 +228,42 @@ INSERT INTO `meeting_sequence` (`id`) VALUES
 (57),
 (58),
 (59),
-(60);
+(60),
+(61),
+(62),
+(63),
+(64),
+(65),
+(66),
+(67),
+(68),
+(69),
+(70),
+(71),
+(72),
+(73),
+(74),
+(75),
+(76),
+(77),
+(78),
+(79),
+(80),
+(81),
+(82),
+(83),
+(84),
+(85),
+(86),
+(87),
+(88),
+(89),
+(90),
+(91),
+(92),
+(93),
+(94),
+(95);
 
 -- --------------------------------------------------------
 
@@ -203,18 +280,20 @@ CREATE TABLE `user` (
   `salt` varchar(255) NOT NULL COMMENT '密碼加鹽',
   `role` enum('admin','employee') NOT NULL DEFAULT 'employee',
   `lineid` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- 傾印資料表的資料 `user`
 --
 
 INSERT INTO `user` (`id`, `name`, `account`, `password`, `salt`, `role`, `lineid`) VALUES
-('A121', 'asdf', 'kzxcv', '607015c6ac3c190d38b056d548ccbd546f9dbd1bed77f750b790e725fd6d0ed69a6bd81185c68ac4d9b5054c7f8b1c9a38bde90d7058664773bd09fda1cd5a34', 'fb10e6f1325081d30104e420baba2433', 'employee', NULL),
-('A124', 'uuu', 'asdfgh', 'dcdfe1e77ec35072007f8944428ae364eae257f9a6faeba628275fb1e067d1075944bb38454a5b21ea058745e4620f2f200fcc8f545407e6791cc6c41d5326e9', '41fb705682d413ca6496a9a38182567c', 'employee', NULL),
-('A125', 'asdf', 'zxcv', 'c9d12d8edc28ae8738c0e296bb114065e6dd51dbac7f951acf12a37d29425364a9137c05d53f3a89965dcb770910e00cb302b60abaf1fdecc6f2f5871567023d', 'f3d78e0bc37fb5b4a69299ab047c9fbe', 'admin', NULL),
+('A121', 'asdf', 'kzxcv', '607015c6ac3c190d38b056d548ccbd546f9dbd1bed77f750b790e725fd6d0ed69a6bd81185c68ac4d9b5054c7f8b1c9a38bde90d7058664773bd09fda1cd5a34', 'fb10e6f1325081d30104e420baba2433', 'employee', 'U14720a2f5c3a27e2a6eb9a07d3e7fe4a'),
+('A123', '123', '123', 'b7a68e6138320d008e9dd3df3fc56d8205514fae1fc652040dd2b4ef6cc0b0e73462321517b2ffdbc89b8bd85e2f8eaf6957e7e80fbdb594f10b9f4eef702014', 'de6951aca4a4208399613116d5be82c7', 'employee', 'U8f863d96710e9bff42c1aa06a7f070d3'),
+('A124', 'uuu', 'asdfgh', 'dcdfe1e77ec35072007f8944428ae364eae257f9a6faeba628275fb1e067d1075944bb38454a5b21ea058745e4620f2f200fcc8f545407e6791cc6c41d5326e9', '41fb705682d413ca6496a9a38182567c', 'employee', 'U7f87c27ddbf591759cf39bd2b93fd5e6'),
+('A125', 'asdf', 'zxcv', 'c9d12d8edc28ae8738c0e296bb114065e6dd51dbac7f951acf12a37d29425364a9137c05d53f3a89965dcb770910e00cb302b60abaf1fdecc6f2f5871567023d', 'f3d78e0bc37fb5b4a69299ab047c9fbe', 'admin', 'U8f863d96710e9bff42c1aa06a7f070d3'),
 ('D123', 'd', 'd', '48cef256a29d595dc0f240dff0c64b30279679ffe0d18e891cb1c42d656f7bebd94a70518a1c8dd7b862d15c41d3dc61d5b854873762770a73c7308ccc1bbb5f', 'e74cba17935a15d81317118b809cc126', 'admin', NULL),
-('P555', 'dllm', 'knnzxc', 'd5c2396b9bc38914e5ac2b6fb563ac3e3a543edcc640a9f22cdc28f591782f06c91a43446db3a6f8b5033047c2bf1b269a838b6cc5c8d6a90bd0f30d09d62442', '25aaeab029ad36ffe69a76ab2ea369b6', 'employee', NULL);
+('P555', 'dllm', 'knnzxc', 'd5c2396b9bc38914e5ac2b6fb563ac3e3a543edcc640a9f22cdc28f591782f06c91a43446db3a6f8b5033047c2bf1b269a838b6cc5c8d6a90bd0f30d09d62442', '25aaeab029ad36ffe69a76ab2ea369b6', 'employee', 'Uf534e6a58705c9fbdf629c58ca56b7b3'),
+('R024', 'paox', 'qwerty123', '1e377ab51e7516c9936e0785de5317b4e450c427852896949804e825f9157c8000f21c014aafe04f0d860cc884e8991a76ddd43cb6994ec902362a992ffb7732', 'fdef79445baf13d6bd1c71ee783fcfbd', 'employee', 'Udf8cc32537d4c7bf90a4e32df73640a7');
 
 --
 -- 已傾印資料表的索引
@@ -263,13 +342,13 @@ ALTER TABLE `user`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `meeting_member`
 --
 ALTER TABLE `meeting_member`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `meeting_sequence`
 --
 ALTER TABLE `meeting_sequence`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=96;
 
 --
 -- 已傾印資料表的限制式
@@ -279,15 +358,15 @@ ALTER TABLE `meeting_sequence`
 -- 資料表的限制式 `meeting`
 --
 ALTER TABLE `meeting`
-  ADD CONSTRAINT `FK_a00e1c5af408052c92418ea9bec` FOREIGN KEY (`meetingRoomId`) REFERENCES `meeting_room` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `FK_fe775c687e31ff7950e35650c40` FOREIGN KEY (`creatorId`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `FK_a00e1c5af408052c92418ea9bec` FOREIGN KEY (`meetingRoomId`) REFERENCES `meeting_room` (`id`),
+  ADD CONSTRAINT `FK_fe775c687e31ff7950e35650c40` FOREIGN KEY (`creatorId`) REFERENCES `user` (`id`);
 
 --
 -- 資料表的限制式 `meeting_member`
 --
 ALTER TABLE `meeting_member`
-  ADD CONSTRAINT `FK_34b5654c29e64f0a80ff5f2c691` FOREIGN KEY (`participantId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  ADD CONSTRAINT `FK_58f476a39e1b8c0ea2ceab23552` FOREIGN KEY (`meetingId`) REFERENCES `meeting` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+  ADD CONSTRAINT `FK_34b5654c29e64f0a80ff5f2c691` FOREIGN KEY (`participantId`) REFERENCES `user` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_58f476a39e1b8c0ea2ceab23552` FOREIGN KEY (`meetingId`) REFERENCES `meeting` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
