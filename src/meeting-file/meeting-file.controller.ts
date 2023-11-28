e7c3cfcb9fe6f3b6153dd707930c284e46705a6a
sobol <nppstcc1001050311@gmail.com>
2023-11-28 20:23:19 +0800

    整理程式、確保下載連接可用

--- src/meeting-file/meeting-file.controller.ts
+++ src/meeting-file/meeting-file.controller.ts
@@ -6,6 +6,7 @@ import {
   HttpStatus,
   Param,
   Post,
+  Req,
   StreamableFile,
   UploadedFiles,
   UseGuards,
@@ -14,60 +15,80 @@ import {
 import { ConfigService } from '@nestjs/config';
 import { AuthGuard } from '@nestjs/passport';
 import { FilesInterceptor } from '@nestjs/platform-express';
+import { Request } from 'express';
 import * as fs from 'fs';
 import { join } from 'path';
+import commonLogger from 'src/common/loggerController';
 import { Result } from 'src/common/standardResult';
 import { MeetingService } from 'src/meeting/meeting.service';
 import { RoleGuard } from 'src/role/role.guard';
 import { MeetingFileService } from './meeting-file.service';
 
 @Controller('meeting-file')
-export class MeetingFileController {
+export class MeetingFileController extends commonLogger {
   constructor(
     private readonly meetingFileService: MeetingFileService,
     private readonly meetingService: MeetingService,
     private readonly configService: ConfigService,
-  ) {}
+  ) {
+    super();
+  }
 
   @Post('/:meetingId')
   @UseGuards(AuthGuard('jwt'))
   @UseGuards(RoleGuard)
+  // @UseInterceptors(meetingFileRoleInterceptor)
   @UseInterceptors(FilesInterceptor('file'))
   async create(
+    @Req() request: Request,
     @Param('meetingId') meetingId: string,
     @UploadedFiles() files: Express.Multer.File[],
   ) {
-    console.log(meetingId);
+    // console.log(meetingId);
     const meeting = await this.meetingService.findOne(meetingId);
     if (meeting.length === 0) {
       throw new HttpException('會議不存在', HttpStatus.NOT_FOUND);
     }
-    console.log(`file: ${files}`);
-    const fileList = files.map(({ fieldname, originalname }, index) => ({
-      id: index,
-      fieldname,
-      name: originalname,
-      download: `http://localhost:5000/meeting-file/download/${meetingId}-${originalname}`,
-    }));
+    // console.log(`file: ${files}`);
+    const port =
+      request.hostname !== 'localhost' ? '' : request.socket.localPort;
+    const fileList = files.map(({ fieldname, originalname }, index) => {
+      return {
+        id: index,
+        fieldname,
+        name: originalname,
+        // reference -> https://stackoverflow.com/a/10349106/15407937
+        download: `//${request.hostname}:${port}/meeting-file/download/${meetingId}-${originalname}`,
+      };
+    });
     return Result.ok(fileList, '上傳成功');
   }
 
   @Get('/:meetingId')
   @UseGuards(RoleGuard)
   @UseGuards(AuthGuard('jwt'))
-  getDownloadList(@Param('meetingId') meetingId: string) {
+  getDownloadList(
+    @Req() request: Request,
+    @Param('meetingId') meetingId: string,
+  ) {
     const path = join(__dirname, '../../', 'uploads');
     try {
       const files = fs.readdirSync(path);
-      console.log(this.configService.get('domain'));
+      const port =
+        request.hostname !== 'localhost' ? '' : request.socket.localPort;
+      this.logger.log(`protocol -> ${request.protocol}`);
+      this.logger.log(`port -> ${request.socket.localPort}`);
       const matchingFiles = files
         .filter((file) => file.includes(meetingId))
-        .map((file, index) => ({
-          id: index,
-          name: file,
-          download: `http://localhost:5000/meeting-file/download/${file}`,
-        }));
-      console.log(matchingFiles);
+        .map((file, index) => {
+          // reference -> https://stackoverflow.com/a/10349106/15407937
+          const url = `//${request.hostname}:${port}/meeting-file/download/${file}`;
+          return {
+            id: index,
+            name: file,
+            download: url,
+          };
+        });
       return Result.ok(matchingFiles, '查詢成功');
     } catch (err) {
       throw new HttpException(
@@ -90,6 +111,7 @@ export class MeetingFileController {
   @Delete('/:path')
   @UseGuards(RoleGuard)
   @UseGuards(AuthGuard('jwt'))
+  // @UseInterceptors(meetingFileRoleInterceptor)
   remove(@Param('path') path: string) {
     const fullPath = join(__dirname, '../../', 'uploads', path);
     if (!fs.existsSync(fullPath)) {
--- src/meeting-file/meeting-file.module.ts
+++ src/meeting-file/meeting-file.module.ts
@@ -1,17 +1,19 @@
 import { Module } from '@nestjs/common';
-import { MeetingFileService } from './meeting-file.service';
-import { MeetingFileController } from './meeting-file.controller';
+import { JwtService } from '@nestjs/jwt';
 import { MulterModule } from '@nestjs/platform-express';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { diskStorage } from 'multer';
 import { MulterHelper } from 'src/common/multerHelper';
-import { MeetingService } from 'src/meeting/meeting.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { MeetingMember } from 'src/meeting-member/entities/meeting-member.entity';
+import { MeetingMemberService } from 'src/meeting-member/meeting-member.service';
+import { MeetingRoom } from 'src/meeting-room/entities/metting-room.entity';
+import { MeetingRoomService } from 'src/meeting-room/meeting-room.service';
 import { Meeting } from 'src/meeting/entities/meeting.entity';
 import { meetingSequence } from 'src/meeting/entities/meeting.sequence';
-import { MeetingRoom } from 'src/meeting-room/entities/metting-room.entity';
+import { MeetingService } from 'src/meeting/meeting.service';
 import { User } from 'src/user/entities/user.entity';
-import { MeetingRoomService } from 'src/meeting-room/meeting-room.service';
-import { JwtService } from '@nestjs/jwt';
+import { MeetingFileController } from './meeting-file.controller';
+import { MeetingFileService } from './meeting-file.service';
 
 @Module({
   imports: [
@@ -21,12 +23,19 @@ import { JwtService } from '@nestjs/jwt';
         filename: MulterHelper.filenameHandler,
       }),
     }),
-    TypeOrmModule.forFeature([Meeting, meetingSequence, MeetingRoom, User]),
+    TypeOrmModule.forFeature([
+      Meeting,
+      meetingSequence,
+      MeetingMember,
+      MeetingRoom,
+      User,
+    ]),
   ],
   controllers: [MeetingFileController],
   providers: [
     MeetingFileService,
     MeetingService,
+    MeetingMemberService,
     MeetingRoomService,
     JwtService,
   ],
